e7bd4d8616a6f0db6db095254b22455235e6d5b3
Jason Inouye <jasonjinouye@gmail.com>
2022-05-02 16:50:19 -0500

    added variables and if statement

--- Program.cs
+++ Program.cs
@@ -9,9 +9,21 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             int myNum = 5;
+            double myDoubleNum = 5.99;
+            bool myBool = false;
+            int x = 20;
+            int y = 18;
             Console.WriteLine("Hello World!");
             Console.WriteLine("The current time is " + DateTime.Now);
             Console.WriteLine("This is an int " + myNum);
+            Console.WriteLine("This is an double " + myDoubleNum);
+            Console.WriteLine("This is an bool " + myBool);
+            Console.WriteLine("x is " + x);
+            Console.WriteLine("y is " + y);
+            if (x > y)
+            {
+                Console.WriteLine("x is greater than y");
+            }
         }
     }
 }
