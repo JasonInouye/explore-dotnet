6fe32f72f362b8da0900307a68f45f7df674f2f0
Jason Inouye <jasonjinouye@gmail.com>
2022-05-02 16:37:16 -0500

    initial c# files

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myNum = 5;
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("The current time is " + DateTime.Now);
+            Console.WriteLine("This is an int " + myNum);
+        }
+    }
+}
+
