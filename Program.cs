037113840e8b1278b0806a9f884be9f668610e88
Jason Inouye <jasonjinouye@gmail.com>
2022-05-02 22:27:31 -0500

    added array object

--- Program.cs
+++ Program.cs
@@ -1,44 +1,80 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using System;
+// using System;
 
-namespace HelloWorld
+// namespace HelloWorld
+// {
+//     class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             int myNum = 5;
+//             double myDoubleNum = 5.99;
+//             bool myBool = false;
+//             int x = 20;
+//             int y = 18;
+//             // Array of strings
+//             string[] candy = { "Kisses", "Reeses", "M&M", "Kit Kat" };
+//             Console.WriteLine("Hello World!");
+//             Console.WriteLine("The current time is " + DateTime.Now);
+//             Console.WriteLine("This is an int " + myNum);
+//             Console.WriteLine("This is an double " + myDoubleNum);
+//             Console.WriteLine("This is an bool " + myBool);
+//             Console.WriteLine("x is " + x);
+//             Console.WriteLine("y is " + y);
+//             // IF Statement
+//             if (x > y)
+//             {
+//                 Console.WriteLine("x is greater than y");
+//             };
+//             //Switch
+//             switch (x)
+//             {
+//                 case 1:
+//                     Console.WriteLine("1 is Less Than 15");
+//                     break;
+//                 case 5:
+//                     Console.WriteLine("5 is Less Than 15");
+//                     break;
+//                 case 15:
+//                     Console.WriteLine("15 is equal to 15");
+//                     break;
+//                 case 20:
+//                     Console.WriteLine("20 is Greater than 15");
+//                     break;
+//             };
+//             Console.WriteLine(candy[0]);
+//             // for loop
+//             foreach (string i in candy)
+//             {
+//                 Console.WriteLine(i);
+//             };
+
+//             // Class - Object
+
+//         }
+//     }
+// };
+
+// class Car
+// {
+//     string color = "red";
+//     static void Main(string[] args)
+//     {
+//         Car myObj = new Car();
+//         Console.WriteLine(myObj.color);
+//     }
+// };
+// Unable to get this to work
+static void MyMethod() 
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            int myNum = 5;
-            double myDoubleNum = 5.99;
-            bool myBool = false;
-            int x = 20;
-            int y = 18;
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("The current time is " + DateTime.Now);
-            Console.WriteLine("This is an int " + myNum);
-            Console.WriteLine("This is an double " + myDoubleNum);
-            Console.WriteLine("This is an bool " + myBool);
-            Console.WriteLine("x is " + x);
-            Console.WriteLine("y is " + y);
-            if (x > y)
-            {
-                Console.WriteLine("x is greater than y");
-            };
-            switch (x) {
-                case 1:
-                    Console.WriteLine("1 is Less Than 15");
-                    break;
-                case 5:
-                    Console.WriteLine("5 is Less Than 15");
-                    break;
-                case 15: 
-                    Console.WriteLine("15 is equal to 15");
-                    break;
-                case 20: 
-                    Console.WriteLine("20 is Greater than 15");
-                    break;
-            }
-        }
-    }
+  Console.WriteLine("I just got executed!");
+}
+
+static void Main(string[] args)
+{
+  MyMethod();
+  MyMethod();
+  MyMethod();
 }
 
